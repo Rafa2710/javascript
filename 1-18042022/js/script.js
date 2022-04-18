65351a4a11e0609582881525cbf9c44b229d5794
RAFAEL SILVA COSTA <rafael.scosta24@senacsp.edu.br>
2022-04-18 12:02:37 -0300

    inicio dos estudos

--- 1-18042022/js/script.js
+++ 1-18042022/js/script.js
@@ -1,28 +1,39 @@
 //janela de alerta criada em javascript
-//alert("Olá, seja bem vindo");
+function bemvindos() {
+  alert("Olá, seja bem vindo");
+}
+
 //vamos solicitar ao usiario que entre com o seu nome
 //utilizaremos o promt associado a uma variavel
-/*var nome ="";
-nome = promt ("Digite seu nome");
-alert(nome.toUpperCase()); //nome em letras maiusculas*/
+function nome() {
+  var nome = "";
+  nome = promt("Digite seu nome");
+  alert(nome.toUpperCase()); //nome em letras maiusculas*/
+}
+function operacoes() {
+  //calcular as 5 operacoes aritmeticas*/
 
-//calcular as 5 operacoes aritmeticas*/
+  var numero1 = prompt("digite um numero");
+  var numero2 = prompt("digite outro numero");
 
-var numero1 = prompt ("digite um numero");
-var numero2 = prompt ("digite outro numero")
+  var soma = parseInt(numero1) + parseInt(numero2);
+  var subtrair = numero1 - numero2;
+  var multiplicar = numero1 * numero2;
+  var dividir = numero1 / numero2;
+  var resto = numero1 % numero2;
 
-var soma = parseInt(numero1) + parseInt (numero2);
-var subtrair = numero1 - numero2;
-var multiplicar = numero1 * numero2;
-var dividir = numero1 / numero2;
-var resto = numero1 % numero2;
 
-console.log(
-     "os resultados são: \n\n"+
-     "soma: "+soma+
-     "\nsubtração: "+subtrair+
-     "\nmultiplicação: "+multiplicar+
-     "\nduvisão: "+dividir+
-     "\nresto: "+resto
-);
-
+document.getElementsByTagName("button")[0].innerHTML = 
+  "os resultados são: <br><br>" +
+    "soma: " +
+    soma +
+    "<br>subtração: " +
+    subtrair +
+    "<br>multiplicação: " +
+    multiplicar +
+    "<br>duvisão: " +
+    dividir +
+    "<br>resto: " +
+    resto;
+}
+    
